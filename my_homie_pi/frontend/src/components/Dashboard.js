d7b8bcd97f8ffbd6c29b45a54efaca1e8fb93590
sharlec <schan543@uwo.ca>
2021-08-23 17:18:59 +0800

    dashboard buttons and modal

--- my_homie_pi/frontend/src/components/Dashboard.js
+++ my_homie_pi/frontend/src/components/Dashboard.js
@@ -15,39 +15,6 @@ import TextField from "@material-ui/core/TextField/TextField";
 import Modal from './Modal/addTask'
 
 
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    height: '100%',
-    width: '100%',
-    backgroundColor: 'black'
-  },
-  image: {
-    backgroundImage:
-      'url(https://media.giphy.com/media/kz76j0UjtPoE4WyhQn/giphy.gif)',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center'
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  avatar: {
-    margin: theme.spacing(1)
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-    color: 'orange'
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
-  }
-}));
-
 export default class Dashboard extends Component {
     constructor(props) {
         super(props);
@@ -57,7 +24,7 @@ export default class Dashboard extends Component {
             // token : localStorage.getItem('access_token'),
             user_ID: null,
             gender: "M",
-            name: null,
+            username: null,
             age: null,
             daily_record: [],
             visible: false,
@@ -89,7 +56,7 @@ export default class Dashboard extends Component {
             data => {
                 if (data.username) {
                     this.setState({
-                        name: data.username,
+                        username: data.username,
                         age: data.age,
                         gender: data.gender,
                     });
@@ -102,91 +69,146 @@ export default class Dashboard extends Component {
 
     }
 
-    handleLogoutButtonPressed() {
-        console.log(this.state);
-        const requestOptions = {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json, text/plain',
-                'Content-Type': 'application/json;charset=UTF-8',
-            },
-            body: JSON.stringify({
-                username: this.state.username,
-                password: this.state.password,
-            }),
-        };
-
-        fetch('/API/login', requestOptions).then((response) => response.json()).then(
-            data => {
-                if (data.access) {
-                    localStorage.setItem('access_token', data.access);
-                    localStorage.setItem('refresh_token', data.refresh);
-
-                    this.props.history.push("dashboard")
-                } else console.log()
-            }
-        )
-    }
+    // handleLogoutButtonPressed() {
+    //     console.log(this.state);
+    //     const requestOptions = {
+    //         method: "POST",
+    //         headers: {
+    //             'Accept': 'application/json, text/plain',
+    //             'Content-Type': 'application/json;charset=UTF-8',
+    //         },
+    //         body: JSON.stringify({
+    //             username: this.state.username,
+    //             password: this.state.password,
+    //         }),
+    //     };
+    //
+    //     fetch('/API/login', requestOptions).then((response) => response.json()).then(
+    //         data => {
+    //             if (data.access) {
+    //                 localStorage.setItem('access_token', data.access);
+    //                 localStorage.setItem('refresh_token', data.refresh);
+    //
+    //                 this.props.history.push("dashboard")
+    //             } else console.log()
+    //         }
+    //     )
+    // }
 
 
 
 //
   render() {
-        const { visible } = this.state
+        const { visible } = this.state;
         return (
-            <div>
-        <Grid container direction="rows" justifyContent="center" alignItems="center" spacing={3} xs = {6}>
-        <Grid item xs={12} align="center">
-          <Typography component="h4" variant="h4">
-            Dashboard
-          </Typography>
+
+    <Grid   container direction="row"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: '100vh' }}
+            xs  ={12}>
+
+      <Grid item align = "center" xs={4}>
+        <Grid container direction="column"
+              align = "center">
+            <Grid item><h3>Dashboard</h3></Grid>
+            <Grid item>username : {this.state.username}</Grid>
+            <Grid item>age : {this.state.age}</Grid>
+            <Grid item>gender : {this.state.gender}</Grid>
+            <Grid item>maybe photo</Grid>
+            <Grid item>
+                <Button color = "primary"
+                        variant="contained"
+                        onClick={this.handleLogoutButtonPressed}
+                        style={{ width: 80}}>
+                 Logout
+             </Button>
         </Grid>
-            <Grid item xs={12} align="center">
-      <div>
-      <h3>{this.user_name}</h3>
-           <p>name: {this.state.name}</p>
-        <p>age: {this.state.age}</p>
-        <p>gender: {this.state.gender}</p>
-         <button onClick={this.showModal}>click here</button>
-          <Modal visible={visible}></Modal>
-
-                  <br/><br/><br/>
-        <h3>The following is the record</h3>
-          <p>task: {this.state.daily_record.task_name}</p>
-      </div>
-            </Grid>
         </Grid>
+      </Grid>
 
-        <Grid container direction="rows" justifyContent="center" alignItems="center" spacing={3} xs = {6}>
 
-        <Grid item xs={12} align="center">
-            <FormControl>
-                <TextField
-                    required={true}
-                    type="text"
-                    inputProps={{
-                        style:{textAlign:"center"},
-                    }}
-                    onChange={this.handleUserNameChange}
-                />
-                <FormHelperText>
-                    <div align = "center">User Name</div>
-                </FormHelperText>
-            </FormControl>
+      <Grid item align="center" xs={4}>
+        <Grid container
+              direction="column">
+            <Grid item><h3>Today Agenda</h3></Grid>
+             <Grid item>4</Grid>
         </Grid>
+     </Grid>
 
-       <Grid item xs={12} align="center">
-            <Button color = "primary" variant="contained" onClick={this.handleLogoutButtonPressed} style={{ width: 80}}>
-                Logout
-            </Button>
 
-           <Button color = "secondary" variant="contained" to="/" component={Link} style={{ width: 80}}>
-                Back
-            </Button>
-        </Grid>
+    <Grid item align ="center" xs={4}>
+        <Grid container direction="column" >
+        <Grid item xs = {6} align ="center">Task Showlist</Grid>
+               {/*<Button color = "primary"*/}
+                       {/*variant="contained"*/}
+                       {/*onClick ={this.showModal()}*/}
+                       {/*// style={{ width: 80}}*/}
+               {/*>*/}
+                {/*Add Task*/}
+            {/*</Button>*/}
+            <Grid item><button onClick={this.showModal}>click here</button>
+            <Modal visible={visible}></Modal></Grid>
 
-        </Grid>
-            </div>
+
+    </Grid>
+    </Grid>
+
+</Grid>
+
+
+      //   <Grid container direction="rows" justifyContent="center" alignItems="center" spacing={3} xs = {6}>
+      //   <Grid item xs={12} align="center">
+      //     <Typography component="h4" variant="h4">
+      //       Dashboard
+      //     </Typography>
+      //   </Grid>
+      //       <Grid item xs={12} align="center">
+      // <div>
+      // <h3>{this.user_name}</h3>
+      //      <p>name: {this.state.name}</p>
+      //   <p>age: {this.state.age}</p>
+      //   <p>gender: {this.state.gender}</p>
+      //    <button onClick={this.showModal}>click here</button>
+      //     <Modal visible={visible}></Modal>
+      //
+      //             <br/><br/><br/>
+      //   <h3>The following is the record</h3>
+      //     <p>task: {this.state.daily_record.task_name}</p>
+      // </div>
+      //       </Grid>
+      //   </Grid>
+
+       //  <Grid container direction="rows" justifyContent="center" alignItems="center" spacing={3} xs = {6}>
+       //
+       //  <Grid item xs={12} align="center">
+       //      <FormControl>
+       //          <TextField
+       //              required={true}
+       //              type="text"
+       //              inputProps={{
+       //                  style:{textAlign:"center"},
+       //              }}
+       //              onChange={this.handleUserNameChange}
+       //          />
+       //          <FormHelperText>
+       //              <div align = "center">User Name</div>
+       //          </FormHelperText>
+       //      </FormControl>
+       //  </Grid>
+       //
+       // <Grid item xs={12} align="center">
+       //      <Button color = "primary" variant="contained" onClick={this.handleLogoutButtonPressed} style={{ width: 80}}>
+       //          Logout
+       //      </Button>
+       //
+       //     <Button color = "secondary" variant="contained" to="/" component={Link} style={{ width: 80}}>
+       //          Back
+       //      </Button>
+       //  </Grid>
+       //
+       //  </Grid>
+
 
 
     );
